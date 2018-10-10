aa55777f5820fe5d97bd4e30dac1ca4b960d9523
Henry <chocolatkey@gmail.com>
2018-10-10 09:53:53 -0700

    removed dev mode, add loading spinner

--- consts.js
+++ consts.js
@@ -2,6 +2,5 @@
 const pkg = require("./package.json");
 module.exports = {
     __VERSION__: JSON.stringify(pkg.version + "-" + (new Date).getTime()),
-    __NAME__: JSON.stringify(pkg.name),
-    __DEV__: true
+    __NAME__: JSON.stringify(pkg.name)
 };
