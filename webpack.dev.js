1377c1dd22d370dfd703407e0d7f5ffb2a76b971
daszgfz <dasZGFz@protonmail.com>
2019-03-28 13:46:23 -0700

    Remove ts-loader from dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -36,11 +36,7 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            loader: "ts-loader"
-        }, {
-            test: /\.js$/,
+            test: [/\.tsx?$/, /\.js$/],
             exclude: /node_modules/,
             loader: "babel-loader"
         },
