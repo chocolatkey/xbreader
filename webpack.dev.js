22faede9cfffec1d90042c805c6871d0ce08ca87
daszgfz <dasZGFz@protonmail.com>
2019-04-19 22:49:27 -0700

    fix: remove info messages from dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -18,7 +18,8 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, "bin"),
         compress: true,
-        port: 8080
+        port: 8080,
+        noInfo: true
     },
     entry: {
         xbstyles: [
